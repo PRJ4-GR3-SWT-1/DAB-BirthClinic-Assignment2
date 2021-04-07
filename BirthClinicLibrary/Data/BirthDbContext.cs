0b71d847d64f61fa83e08064cf97fa7fbb740a76
Filip <f-r-c@LAPTOP-BO73C4EV>
2021-04-07 10:58:28 +0200

    Add project files.

---
+++ BirthClinicLibrary/Data/BirthDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BirthClinicLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Extensions;
+
+
+namespace EFModels.Data
+{
+    public class MaternityDbContext : DbContext
+    {
+        //public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(localdb)\DABServer;Initial Catalog=MaternityClinic;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+
+
+    }
+}
---
+++ ConsoleAplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
